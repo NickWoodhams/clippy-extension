06af8be7378e02238aaae5c76dbf6482bf01ae47
Nick Woodhams <nicholas.woodhams@gmail.com>
2015-05-12 07:35:02 -0400

    test 2 of clippy

--- js/clippy.js
+++ js/clippy.js
@@ -6,6 +6,75 @@ serialize = function(obj) {
 };
 
 
+/**
+ * Writes an image into a canvas taking into
+ * account the backing store pixel ratio and
+ * the device pixel ratio.
+ *
+ * @author Paul Lewis
+ * @param {Object} opts The params for drawing an image to the canvas
+ */
+function drawImage(opts, callback) {
+    // create image
+    var pic = new Image();
+    // get the canvas and context
+    var canvas = document.createElement("canvas"),
+        context = canvas.getContext('2d'),
+        image = opts.image,
+
+    // now default all the dimension info
+        srcx = opts.srcx || 0,
+        srcy = opts.srcy || 0,
+        srcw = opts.srcw || image.naturalWidth,
+        srch = opts.srch || image.naturalHeight,
+        desx = opts.desx || srcx,
+        desy = opts.desy || srcy,
+        desw = opts.desw || srcw,
+        desh = opts.desh || srch,
+        auto = opts.auto,
+
+    // finally query the various pixel ratios
+        devicePixelRatio = window.devicePixelRatio || 1,
+        backingStoreRatio = context.webkitBackingStorePixelRatio ||
+                            context.mozBackingStorePixelRatio ||
+                            context.msBackingStorePixelRatio ||
+                            context.oBackingStorePixelRatio ||
+                            context.backingStorePixelRatio || 1,
+
+        ratio = devicePixelRatio / backingStoreRatio;
+
+    // ensure we have a value set for auto.
+    // If auto is set to false then we
+    // will simply not upscale the canvas
+    // and the default behaviour will be maintained
+    if (typeof auto === 'undefined') {
+        auto = true;
+    }
+
+    // upscale the canvas if the two ratios don't match
+    if (auto && devicePixelRatio !== backingStoreRatio) {
+
+        var oldWidth = canvas.width;
+        var oldHeight = canvas.height;
+
+        canvas.width = oldWidth * ratio;
+        canvas.height = oldHeight * ratio;
+
+        canvas.style.width = oldWidth + 'px';
+        canvas.style.height = oldHeight + 'px';
+
+        // now scale the context to counter
+        // the fact that we've manually scaled
+        // our canvas element
+        context.scale(ratio, ratio);
+
+    }
+
+    context.drawImage(pic, srcx, srcy, srcw, srch, desx, desy, desw, desh);
+    callback(canvas.toDataURL());
+}
+
+
 function resizeImage(url, width, height, left, top, callback) {
     var sourceImage = new Image();
 
@@ -181,12 +250,18 @@ function activatePicker() {
             screenshot = response.imgSrc;
             console.log(screenshot);
 
-            screenshot_resized = resizeImage(
-                response.imgSrc,
-                e.target.clientWidth,
-                e.target.clientHeight,
-                html_jq.offset().left - $(window).scrollLeft(),
-                html_jq.offset().top - $(window).scrollTop(),
+            var opts = {
+                image: response.imgSrc,
+                srcx: html_jq.offset().left - $(window).scrollLeft(),
+                srcy: html_jq.offset().left - $(window).scrollTop(),
+                desx: 0,
+                desy: 0,
+                desw: e.target.clientWidth,
+                desh: e.target.clientHeight,
+            }
+
+            screenshot_resized = drawImage(
+                opts,
                 function(response) {
                     console.log("screenshot_resized", response)
                     jQuery("#snippet-loader").show();
