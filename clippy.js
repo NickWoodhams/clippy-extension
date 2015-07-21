65635c18efc8ab7aa62ad6fd2e63cc8ac541f8ea
Nick Woodhams <nicholas.woodhams@gmail.com>
2015-07-20 23:54:48 -0400

    removed unused functions, bumped version number

--- clippy.js
+++ clippy.js
@@ -1,16 +1,4 @@
 
-Element.prototype.remove = function() {
-    this.parentElement.removeChild(this);
-};
-
-NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
-    for(var i = this.length - 1; i >= 0; i--) {
-        if(this[i] && this[i].parentElement) {
-            this[i].parentElement.removeChild(this[i]);
-        }
-    }
-};
-
 serialize = function(obj) {
     var str = [];
     for (var p in obj)
